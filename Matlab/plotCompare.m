d62c0b9f57a33084c74439d042fa0d06c1f4f7a7
lsangild <lsangild@gmail.com>
2018-05-14 13:05:22 +0200

    Added scripts to plot frequency comparisons of speakers

--- Matlab/plotCompare.m
+++ Matlab/plotCompare.m
@@ -2,21 +2,19 @@ load Measurements.mat
 
 % Normalized frequency
 Y0 = fft(PG_closed);
-Y1 = fft(PG_hole_bot);
-Y2 = fft(PG_BR_bot);
+Y1 = fft(PK_closed);
 
 % To dB_SPL
 Y0 = 20*log10(Y0./20e-6);
 Y1 = 20*log10(Y1./20e-6);
-Y2 = 20*log10(Y2./20e-6);
 
 figure
+xlabel('Frequency (Hz)');
 semilogx(f(1:end/2), abs(Y0(1:end/2)))
 hold on
 semilogx(f(1:end/2), abs(Y1(1:end/2)))
-semilogx(f(1:end/2), abs(Y2(1:end/2)))
 grid on
 xlabel('Frequency (Hz)');
 ylabel('dB_{SPL}');
-xlim([10, 600]);
-legend('Closed', 'Hole bot', 'BR bot', "location", "southeast");
+xlim([40, 1000]);
+legend('PG Closed', 'PK Closed');
