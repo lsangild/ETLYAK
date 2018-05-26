3acce2dc4ec6d3d28b7087b265d343ebf21e248b
jonaslind10 <jonas.lind12@gmail.com>
2018-05-26 13:00:13 +0200

    DriveUnit closed cabinet

--- Matlab/DriveUnit.m
+++ Matlab/DriveUnit.m
@@ -1,10 +1,10 @@
-classdef DriveUnit < TransferFunctions
+classdef DriveUnit < TransfeRunctions
   properties (Access = private)
     %% Defaults
     % Density of air (kg/m^3)
     rho     = 1.1839;
     % Distance to microphone (m)
-    rF      = 1;
+    R      = 1;
   end
   properties (Access = private)
     %% Data sheet values
@@ -29,12 +29,15 @@ classdef DriveUnit < TransferFunctions
           % L = TRANSFORM(f) Returns the sound pressure.
           setDerivedParameters(obj);
           % p = rho*Sd*Bl*UG/2*pi*Mms*Re
-          
-          A = (obj.rho * obj.Sd * obj.Bl * obj.UG) / (2 * pi * obj.Mms * obj.Re);
-          wS = 2 * pi * obj.fs;
           s = 1i .* 2 .* pi .* f;
-          AL = abs(s.^2 ./ (s.^2 + (1/obj.Qts) .* wS .* s + wS^2));
-          p = (A/obj.rF) .* AL;
+          k0 = (obj.rho * obj.Bl * obj.Sd * obj.UG) / (2 * pi * obj.R * obj.Re * obj.Mms);
+          k1 = ((obj.Bl^2) / (obj.Re * obj.Mms)) + (obj.Rms / obj.Mms);
+          k2 = 1 / (obj.Mms * obj.Cms);
+          p = k0 * (s.^2 / (s.^2 + s .* k1 + k2));
+          
+          %wS = 2 * pi * obj.fs;
+          %AL = abs(s.^2 ./ (s.^2 + (1/obj.Qts) .* wS .* s + wS^2));
+          %p = (A/obj.R) .* AL;
       end
   
       function setParameters(obj, Qts, Bl, Rms, Mms, Cms, Sd, Re, Rnom, fs)
@@ -51,7 +54,7 @@ classdef DriveUnit < TransferFunctions
           % Sd   Effective surface area of driver diaphragm (cm^2) [1.0 < Sd < 1000.0]
           % Re   DC resistance of the voice coil (ohm)
           % Rnom Nominel resistance of drive unit (ohm)
-          % fs   Resonance frequency of the drive unit (Hz)
+          % fS   Resonance frequency of the drive unit (Hz)
           if  (Sd > 1) && (Sd < 1000)
               obj.Sd = Sd/1000;
           else
@@ -76,12 +79,12 @@ classdef DriveUnit < TransferFunctions
           obj.UG = sqrt(1*obj.Rnom);
       end
       
-      function setConstants(obj, rho, rF)
+      function setConstants(obj, rho, R)
         % SETCONSTANTS Change default values of rho, pRef and rf.
         %
         % SETCONSTANTS(rho, pRef, rf)
         % rho  Density of air (kg/m^3)
-        % rf   Distance to microphone (m)
+        % R   Distance to microphone (m)
    
         % Check for correct number of input arguments
         if ~(any([1, 3] == nargin))
@@ -90,10 +93,10 @@ classdef DriveUnit < TransferFunctions
         end
         if nargin == 1
           obj.rho = 1.1839;
-          obj.rF = 1;
+          obj.R = 1;
         else
           obj.rho = rho;
-          obj.rF = rF;
+          obj.R = R;
         end
       end
   end
