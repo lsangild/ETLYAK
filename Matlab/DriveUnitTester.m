0213502120126ff96dd2ad72c955633547863657
jonaslind10 <jonas.lind12@gmail.com>
2018-05-26 13:49:52 +0200

    Change DUTester

--- Matlab/DriveUnitTester.m
+++ Matlab/DriveUnitTester.m
@@ -5,14 +5,14 @@ k = DriveUnit();
 %k.setParameters(0.66, 5.1, 0.6, 7e-3, 1.45e-3, 5.4, 5.6, 8, 50);
 k.setParameters(0.397, 8.2, 1.3036, 14.7e-3, 0.821e-3, 119, 7.2, 8, 45);
 
-k.plotResponse(logspace(0,4,1000));
+k.plotResponse(logspace(1,4,1000));
 
 %%
 % 26.8cm x 20.2cm x 33.0cm
 V = (26.8e-2*20.2e-2*33.0e-2); 
 u = Cabinet(V);
 u.setDriveUnit(k);
-u.plotResponse(logspace(0,4,100));
+u.plotResponse(logspace(1,4,100));
 
 %%
 cf = CrossoverFilter();
